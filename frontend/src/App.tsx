39b1cb259cf6fab5ffb71b1123a45422aac8b03b
Samuel Alves Lima Ferreira <samuel@Samuels-MacBook-Air.local>
2022-09-05 20:05:39 -0300

    First Component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,15 @@
+import NotificationButton from "./components/NotificationButton"
+
 function App() {
 return (
+  <>
   <h1>Hello!!!</h1>
+  <NotificationButton />
+  <NotificationButton />
+  <NotificationButton />
+  <NotificationButton />
+  <NotificationButton />
+  </>
 )
 }
 
